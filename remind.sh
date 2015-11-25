5e7bf3d45c95d23c2690da83fb5d96f352fee0d8
Melissa McEwen <mgmcewen@gmail.com>
2015-11-25 12:52:07 -0600

    adding more

--- remind.sh
+++ remind.sh
@@ -18,6 +18,43 @@ case "$1" in
   Message="drush site-install standard --db-url='mysql://root:vagrant@localhost:3306/DBNAME'"
   ;;
 
+#Server
+"create a symlink")
+  Message="ln -s dirtopointtoto symlinkname"
+  ;;
+
+"restart Apache")
+  Message="sudo apachectl restart"
+  ;;
+
+"set GID")
+  Message="chmod g+s"
+  ;;
+
+#File Management
+
+"rsync remote folder")
+  Message="rsync -r username@server.com:remotedir/  localdir/ "
+  ;;
+
+"rsync options")
+  Message="verbose: -v, compress: -z, recursive: -r, -P: show progress "
+  ;;
+
+"scp")
+  Message="scp username@server.com:/FILEPATH/REMOTEFILE.WHATEVER LOCALFILE.WHATEVER"
+  ;;
+
+"tar")
+  Message="tar -cvzf name.tgz subdir"
+  ;;
+
+"untar")
+  Message="into directory: tar -C /myfolder -xvf yourfile.tar"
+  ;;
+
+
+
 *)
   Message="sorry I don't know that one"
   ;;
